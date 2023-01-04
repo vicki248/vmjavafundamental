bac92798ea754811f4edb9fac027bddd1d3c7a1d
Vicki M <248vicki@gmail.com>
2023-01-04 23:26:36 +0700

    Second Commit

---
+++ src/main/java/com/juaracoding/vmjavafundamental/BuatCommit.java
@@ -0,0 +1,15 @@
+package com.juaracoding.vmjavafundamental;/*
+Created By IntelliJ IDEA 2022.2.3 (Community Edition)
+Build #IU-222.4345.14, built on January 4, 2023
+@Author Vicki M a.k.a. Vicki Mantovani
+Java Developer
+Created on 04/01/2023 23:24
+@Last Modified 04/01/2023 23:24
+Version 1.1
+*/
+
+public class BuatCommit {
+    public static void main(String[] args) {
+        System.out.println("BUAT COMMIT KE GIT HUB");
+    }
+}
